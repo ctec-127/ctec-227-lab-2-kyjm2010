e5a5fedc3c1eb6cea6425d6dacabd42c8cdf22a4
Kyle McDonald <kyjm2010@gmail.com>
2020-05-01 18:04:24 -0700

    Changes made

--- ccookie_eat.php
+++ ccookie_eat.php
@@ -1,4 +1,7 @@
 <?php 
+echo "$_COOKIE[username]</br>";
+echo "$_COOKIE[firstname]</br>";
+echo "$_COOKIE[lastname]</br>";
     if(isset($_COOKIE["cookie"])) {
         foreach($_COOKIE["cookie"] as $key => $val){
             echo $key .' is '.$val."<br>\n";
